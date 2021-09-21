3c16b6ddb178bc9ead5ab5f92d475eb5debba1a6
cscottyb <scottyborland@me.com>
2021-09-21 15:45:50 -0700

    Created scripts to build components and SASS-to-postCSS. Updated project files.

--- src/js/components-build.js
+++ src/js/components-build.js
@@ -1,8 +1,10 @@
 var copy = require('recursive-copy');
 
 var src = 'src/scss/components';
-var dest = 'src/build/buildTemplate/components-build';
-var destDark = 'src/build/buildTemplate/components-build-dark';
+// var dest = 'src/build/buildTemplate/components-build';
+// var destDark = 'src/build/buildTemplate/components-build-dark';
+var dest = 'src/scss/components-build';
+var destDark = 'src/scss/components-build-dark';
 
 var options={
   overwrite: true
@@ -13,13 +15,21 @@ copy(src, dest, options, function(error, results) {
 		console.error('Copy failed: ' + error);
 	} else {
 		console.info('Copied components to component-build. ' + results.length + ' files');
+
+    copy(src, destDark, options, function(error, results) {
+      if (error) {
+        console.error('Copy failed: ' + error);
+      } else {
+        console.info('Copied components to component-build-dark. ' + results.length + ' files');
+      }
+    });
 	}
 });
 
-copy(src, destDark, options, function(error, results) {
-  if (error) {
-    console.error('Copy failed: ' + error);
-  } else {
-    console.info('Copied components to component-build-dark. ' + results.length + ' files');
-  }
-});
+// copy(src, destDark, options, function(error, results) {
+//   if (error) {
+//     console.error('Copy failed: ' + error);
+//   } else {
+//     console.info('Copied components to component-build-dark. ' + results.length + ' files');
+//   }
+// });
